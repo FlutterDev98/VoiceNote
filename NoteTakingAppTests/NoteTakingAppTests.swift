4ad0af86241475e80d9629b4df2319b105b30022
nuriddinjumayev <nuriddinjd1998@gmail.com>
2025-06-12 17:37:42 +0500

    Initial Commit

---
+++ NoteTakingAppTests/NoteTakingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingAppTests.swift
+//  NoteTakingAppTests
+//
+//  Created by Nuriddin Jumayev on 12/06/25.
+//
+
+import Testing
+@testable import NoteTakingApp
+
+struct NoteTakingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
